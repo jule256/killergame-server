14f5670ec6dbcfc66262eae6bb0f17df74dd2dd5
Julian Mollik <github@mollik.net>
2015-10-31 17:30:03 +0100

    added constants

---
+++ config/constants.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = {
+
+    // game status constants
+    status: {
+        inprogress: 'inprogress',
+        finished: 'finished',
+        prestart: 'prestart'
+    },
+
+    // game result constants
+    result: {
+        draw: 'draw',
+        win_player1: 'win_player1',
+        win_player2: 'win_player2',
+        forfeit_player1: 'forfeit_player1',
+        forfeit_player2: 'forfeit_player2'
+    },
+
+    // player constants
+    player1: 'player1',
+    player2: 'player2',
+
+    // token constants
+    token: {
+        player1: 'x',
+        player2: 'o',
+    }
+};

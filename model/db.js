f3f36f42c540f40cba2895218d97a052abdf09cd
Julian Mollik <github@mollik.net>
2015-11-08 22:05:53 +0100

    console outputs

--- model/db.js
+++ model/db.js
@@ -3,13 +3,13 @@
 var mongoose = require('mongoose'),
     config = require('../config/config');
 
-// console.log('connecting to mongoDb:', config.database);
+console.log('connecting to mongoDb:', config.database);
 
 mongoose.connect(config.database, function(err) {
     if (err) {
         throw err;
     }
-//  else {
-//      console.log('connected');
-//  }
+  else {
+      console.log('connected');
+  }
 });

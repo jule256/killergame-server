dce6f8f83d95f5d639d5b9d4f6341e2c5bccbaa3
Julian Mollik <github@mollik.net>
2015-11-08 13:57:37 +0100

    removed serve-favicon

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 
 var express = require('express'),
     path = require('path'),
-//  favicon = require('serve-favicon'),
     logger = require('morgan'),
     cookieParser = require('cookie-parser'),
     bodyParser = require('body-parser'),

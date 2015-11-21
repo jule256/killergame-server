1fe0c417c17105e2f161da2159b5557b88d2014f
dsuesstr <dominik.suesstrunk@gmail.com>
2015-11-21 13:10:19 +0100

    Enable CORS

--- app.js
+++ app.js
@@ -21,6 +21,12 @@ var routes = require('./routes/index'),
 
 var app = express();
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));

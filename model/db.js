c7213adf5bf6f0a041a41c983a47b121ba8f3c2f
Julian Mollik <github@mollik.net>
2015-11-24 20:52:29 +0100

    removed development code

--- model/db.js
+++ model/db.js
@@ -3,13 +3,8 @@
 var mongoose = require('mongoose'),
     config = require('../config/config');
 
-// console.log('connecting to mongoDb:', config.database);
-
 mongoose.connect(config.database, function(err) {
     if (err) {
         throw err;
     }
-//  else {
-//      console.log('connected');
-//  }
 });

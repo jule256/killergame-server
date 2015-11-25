4b08098ec859f6da0310f1369097f0eb16e56aca
Julian Mollik <github@mollik.net>
2015-11-25 21:28:56 +0100

    changed response format

--- routes/register.js
+++ routes/register.js
@@ -84,9 +84,7 @@ getList = function(req, res, next) {
         // resolve callback
         res.format({
             json: function() {
-                res.json({
-                    players: players
-                });
+                res.json(players);
             }
         });
     }, function(error) {

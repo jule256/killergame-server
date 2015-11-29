ef9ece9439eec404c5b33634a6788d2768285dc9
Julian Mollik <github@mollik.net>
2015-11-29 16:32:30 +0100

    development

--- app.js
+++ app.js
@@ -1,33 +1,28 @@
 'use strict';
 
 var express = require('express'),
-    path = require('path'),
     cors = require('cors'),
-    logger = require('morgan'),
     cookieParser = require('cookie-parser'),
     bodyParser = require('body-parser'),
-    morgan = require('morgan'); // for better logging
 
-var db = require('./model/db'), // sets up database connection
+    db = require('./helper/db'), // sets up database connection
     playerModel = require('./model/player'),
-    gameModel = require('./model/game');
+    gameModel = require('./model/game'),
 
-var routes = require('./routes/index'),
+    routes = require('./routes/index'),
     register = require('./routes/register'),
     player = require('./routes/player'),
     game = require('./routes/game'),
     login = require('./routes/login'),
     ping = require('./routes/ping'),
-    dev = require('./routes/dev');
+    dev = require('./routes/dev'),
 
-var app = express();
+    app = express();
 
 app.use(cors());
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/register', register);
@@ -44,15 +39,14 @@ app.use(function(req, res, next) {
     next(err);
 });
 
-// error handlers
-
 // development error handler
-// will print stacktrace
+// will print stacktrace in "development" environment
 if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
-        console.log('error-message', err.message);
-        console.log('error', err);
+
+        console.error('error-message', err.message);
+        console.error('error', err);
 
         res.format({
             json: function() {
@@ -69,10 +63,7 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    console.log('error:', err.message);
+    console.error('error:', err.message);
 });
-
-// for better logging
-// app.use(morgan('dev'));
 
 module.exports = app;
